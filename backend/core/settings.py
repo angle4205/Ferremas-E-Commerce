7740651891bfd5a9ceac746a5ce0693c64693f05
angle4205 <angomez4205@gmail.com>
2025-06-05 13:40:51 -0400

    Soporte para MIME type en el servidor del backend para servir correctamente el front

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -10,8 +10,13 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import mimetypes
 from pathlib import Path
 import os
+
+mimetypes.init() 
+mimetypes.add_type("application/javascript", ".js", True)
+mimetypes.add_type("text/css", ".css", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
